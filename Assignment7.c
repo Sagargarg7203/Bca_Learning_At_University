151332ad0c652cd5867b812f73a9907781a4a742
Sagargarg7203 <127842809+Sagargarg7203@users.noreply.github.com>
2023-04-27 16:20:15 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -2,5 +2,21 @@
 
 int main()
 {
+   char str[] = "sagar garg";
+  int l = 0,i;
+  while (str[l] != '\0')
+  {
+    l++;
+  }
+  printf("Length of string is %d\n",l);
+  
+  char reserved[l];
+  for (int i=0;i<l;i++) 
+  {
+    reserved[i] = str[l-1-i];
+  }
+  reserved[l] = '\0';
+  printf("Reversed string is %s",reserved);
+
   return 0;
 }
