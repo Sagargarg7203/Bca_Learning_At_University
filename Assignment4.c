85542242788648192b47345a52e3d8ec8981fed9
Sagargarg7203 <127842809+Sagargarg7203@users.noreply.github.com>
2023-04-27 15:59:59 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,6 +2,45 @@
 
 int main()
 {
+  int mtr[3][3];
+	int r,c,max[3],temp;
+	printf("enter the elements in array of 3*3:\n");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			scanf("%d",&mtr[r][c]);
+		}
+	}
+	
+	printf("\n");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			printf("%d ",mtr[r][c]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	
+	for(r=0;r<=2;r++)
+	{
+		temp=mtr[r][0];
+		for(c=1;c<=2;c++)
+		{
+			if(mtr[r][c]>temp)
+			{
+				temp=mtr[r][c];
+			}
+		}
+		max[r]=temp;
+	}
+	printf("\n");
+	for(r=0;r<=2;r++)
+	{
+		printf("%d\n",max[r]);
+    }
   
   return 0;
 }
